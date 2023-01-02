f8849fbadbdf375b42837f58655331c7197b4d37
Daniil Hirchyts <78032626+Daniil-Hirchyts@users.noreply.github.com>
2023-01-02 23:19:40 +0100

    Extension 3 - final modifs

--- Extension-3/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module application.extension3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens application.extension3 to javafx.fxml;
-    exports application.extension3;
-}
